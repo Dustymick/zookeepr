4627b682615db935f70d21f838710326dbc4672f
DustyMick <damicklo5@gmail.com>
2021-02-26 15:09:25 -0600

    implemented front end

--- server.js
+++ server.js
@@ -10,6 +10,8 @@ app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
 app.use(express.json());
 
+app.use(express.static('zookeepr-public'));
+
 function filterByQuery(query, animalsArray) {
     let personalityTraitsArray = [];
     // Note that we save the animalsArray as filteredResults here:
@@ -114,7 +116,17 @@ app.post('/api/animals', (req, res) => {
   }
 });
 
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './zookeepr-public/index.html'));
+});
 
+app.get('/animals', (req, res) => {
+  res.sendFile(path.join(__dirname, './zookeepr-public/animals.html'));
+});
+
+app.get('/zookeepers', (req, res) => {
+  res.sendFile(path.join(__dirname, './zookeepr-public/zookeepers.html'));
+});
 
 //this method was used to make our server listen to the request//
 app.listen(PORT, () => {
