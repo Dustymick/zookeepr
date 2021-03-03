573cc16d774330d9fc23e6b19d788485a5ac4d8e
DustyMick <damicklo5@gmail.com>
2021-03-03 09:34:53 -0600

    view animals route fixed

--- server.js
+++ server.js
@@ -1,29 +1,18 @@
-const apiRoutes = require('./routes/apiRoutes');
-const htmlRoutes = require('./routes/htmlRoutes');
-const fs = require('fs');
-const path = require('path');
-const { animals } = require('./data/animals');
 const express = require('express');
+
 const PORT = process.env.PORT || 3001;
 const app = express();
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
 
-// Both of these app,use middleware functions need to be setup everytime you create a server that is looking to accept POST data. parse incoming string or array data. This method is built into Express.js//
 app.use(express.urlencoded({ extended: true }));
-// parse incoming JSON data
 app.use(express.json());
+app.use(express.static('public'));
 
+// Use apiRoutes
 app.use('/api', apiRoutes);
 app.use('/', htmlRoutes);
 
-app.use(express.static('zookeepr-public'));
-
-
-
-
-
-
-
-//this method was used to make our server listen to the request//
 app.listen(PORT, () => {
-    console.log(`API server now on port ${PORT}!`);
+  console.log(`API server now on port ${PORT}!`);
 });
--- zookeepr-public/assets/js/zookeeper.js
+++ zookeepr-public/assets/js/zookeeper.js
@@ -1,4 +1,3 @@
-const $zookeeperForm = document.querySelector("#zookeeper-form");
 const $displayArea = document.querySelector('#display-area');
 
 const printResults = resultArr => {
@@ -21,17 +20,11 @@ const printResults = resultArr => {
   $displayArea.innerHTML = animalHTML.join('');
 };
 
-const getZookeepers = (formData = {}) => {
-  let queryUrl = '/api/zookeepers?';
-
-  Object.entries(formData).forEach(([key, value]) => {
-    queryUrl += `${key}=${value}&`;
-  });
-
-  fetch(queryUrl)
+const getZookeepers = () => {
+  fetch('/api/zookeepers')
     .then(response => {
       if (!response.ok) {
-        return alert(`Error: ${response.statusText}`);
+        return alert('Error: ' + response.statusText);
       }
       return response.json();
     })
@@ -41,18 +34,4 @@ const getZookeepers = (formData = {}) => {
     });
 };
 
-const handleGetZookeepersSubmit = event => {
-  event.preventDefault();
-  const nameHTML = $zookeeperForm.querySelector('[name="name"]');
-  const name = nameHTML.value;
-
-  const ageHTML = $zookeeperForm.querySelector('[name="age"]');
-  const age = ageHTML.value;
-
-  const zookeeperObject = { name, age };
-
-  getZookeepers(zookeeperObject);
-};
-
-$zookeeperForm.addEventListener('submit', handleGetZookeepersSubmit);
 getZookeepers();
